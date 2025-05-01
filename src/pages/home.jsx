f03127c4ba94986520a392159c3f0d7963adb6cb
sadegh <sadeghakbary1381@gmail.com>
2025-05-01 20:01:08 +0330

    Resolve merge conflicts

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import Typography from "bootstrap"
+
+
+export default function Information() {
+    
+    return(
+        <>
+        <Typography variant = "h1">
+            <h1>Resume</h1>
+            <p>نام :صادق</p>
+            <p>نام اکبری :صادق</p>
+            <p>سن :صادق</p>
+        </Typography >
+        </>
+    )
+}
